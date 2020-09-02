320d0c66a3511cbfd166677805290428ee151155
Rishabh391 <Rishabhjain5801@gmail.com>
2020-09-02 11:01:02 +0000

    Added for loop

--- Hello.java
+++ Hello.java
@@ -2,8 +2,11 @@ public class Hello{
 	
 	public static void main(String[] args){
 		
-		System.out.println("Hello World ....");
+		for(int i=0;i<11;i++){	
+			
+			System.out.println("Hello World ...." +i);
 
+		}
 	}
 }
 	
